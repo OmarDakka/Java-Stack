42f7d959ff2fc3580fb4106845bc0a1a5ad38dbb
OmarDakka <omardakka42@gmail.com>
2021-06-09 01:25:19 +0300

    First java program

---
+++ JavaFun/FirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram {
+	public static void main(String[] args) {
+			System.out.println("My name is Omar Daqah");
+			System.out.println("I am 23 years old");
+			System.out.println("My hometown is Tulkarem,PS");
+		}	
+}
