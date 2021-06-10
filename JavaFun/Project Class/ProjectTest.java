d3f68efabb3ad4ba669c5c8968ab94fe7f893958
OmarDakka <omardakka42@gmail.com>
2021-06-10 16:19:26 +0300

    Project Class

---
+++ JavaFun/Project Class/ProjectTest.java
@@ -0,0 +1,8 @@
+public class ProjectTest{
+	public static void main(String[] args) {
+		Project test = new Project();
+		test.setName("Omario");
+		test.setDescription("OMARIO IZ BEST");
+		System.out.println(test.elevatorPitch());
+	}
+}
