c8ff38d1f5d1cdc26951746b187f2783a4461053
OmarDakka <omardakka42@gmail.com>
2021-06-15 12:24:58 +0300

    hello human

---
+++ springProjects/hellohuman/src/main/java/com/codingdojo/secondproject/HellohumanApplication.java
@@ -0,0 +1,18 @@
+package com.codingdojo.secondproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+
+public class HellohumanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellohumanApplication.class, args);
+		
+	}
+
+}
